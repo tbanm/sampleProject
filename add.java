f9e132a2b35462ed6222ebdd458eaf9b8a009333
tbanm <tanmoy00a@gmail.com>
2023-08-16 12:29:58 +0530

    updated to multiplication

--- add.java
+++ add.java
@@ -1,8 +1,8 @@
 public class add {
     public static void main(String[] args) {
-        int a =10, b=30;
+        int a =30, b=50;
         int sum=0;
-        sum=a+b;
+        sum=a*b;
         System.out.println(sum);
     }
     
